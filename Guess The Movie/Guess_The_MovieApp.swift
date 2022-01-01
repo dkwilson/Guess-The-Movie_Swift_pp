05ae226662d2f9a76523edbe476e50d24087223a
dkwilson <general@ytechnology.net>
2021-12-31 22:08:09 -0500

    Initial Commit

---
+++ Guess The Movie/Guess_The_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_MovieApp.swift
+//  Guess The Movie
+//
+//  Created by Dkeith Wilson on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
