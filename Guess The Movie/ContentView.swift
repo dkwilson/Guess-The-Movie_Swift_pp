58392d449b6f9e6c0b362d07177aa309eedfabff
dkwilson <general@ytechnology.net>
2021-12-31 22:49:48 -0500

    corrected button for spiderman

--- Guess The Movie/ContentView.swift
+++ Guess The Movie/ContentView.swift
@@ -6,11 +6,63 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Guess These Movies")
+                .font(.system(size: 30))
+                .fontWeight(.heavy)
+                .foregroundColor(.gray)
+            
+            Text("Can you guess the movie from these emojis?")
+                .font(.system(size: 15))
+            
+            Text("Tap the button to find out the answer")
+                .font(.system(size: 15))
+            
+        Button {
+            speak(text: "Back To The Future")
+        } label: {
+            Text("👨🏾👨🏽‍⚕️🚗⌚️")
+                .padding()
+                .background(.green)
+                .cornerRadius(20)
+        }
+            Button {
+                speak(text: "Frozen")
+            } label: {
+                Text("🏰👯‍♀️❄️⛄️")
+                    .padding()
+                    .background(.yellow)
+                    .cornerRadius(20)
+            }
+            Button {
+                speak(text: "Ocean's Eleven")
+            } label: {
+                Text("🌊1️⃣1️⃣")
+                    .padding()
+                    .background(.blue
+                    )
+                    .cornerRadius(20)
+            }
+            Button {
+                speak(text: "Spider Man")
+            } label: {
+                Text("🕷🏃🏽‍♂️")
+                    .padding()
+                    .background(.brown
+                    )
+                    .cornerRadius(20)
+            }
+        }
+    }
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
     }
 }
 
